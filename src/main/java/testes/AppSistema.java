aac5da07788e7be0bf1b9046d63a9ebee4926006
Raihunter <hilaryoncx@gmail.com>
2021-01-26 11:36:03 -0300

    Progresso

---
+++ src/main/java/testes/AppSistema.java
@@ -0,0 +1,15 @@
+package testes;
+
+import telas.Login.java;
+
+/**
+ *
+ * @author raimundo
+ */
+public class AppSistema {
+    
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+    }
+}
